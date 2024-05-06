ec6e93dc2f3303f1662f8941359502d286e57ed0
Rei Rigos <mcrei1018@gmail.com>
2024-05-06 18:12:33 +0800

    Creating a Condition if not in the Options

--- calculator.py
+++ calculator.py
@@ -21,3 +21,5 @@ def calculator():
             print("3. Multiplication (*) ")
             print("4. Division (/)")
             choice = int(input("Enter the Operation from 1-4 (1,2,3,4): "))
+
+            if choice not in [1, 2, 3, 4]:
