3784635ece67659d1885727bef5f8f23c087e736
Rei Rigos <mcrei1018@gmail.com>
2024-05-06 18:53:03 +0800

    Getting the Zero Division Error

--- calculator.py
+++ calculator.py
@@ -40,3 +40,6 @@ def calculator():
         except ValueError:
             error_message = "You Have Entered an Invalid Character, Number, or Choice"
             print("ValueError: " + error_message)
+
+        except ZeroDivisionError:
+            
