f30ac2d792ff6f868ce87fc304cc397ad19f1651
Rei Rigos <mcrei1018@gmail.com>
2024-05-06 18:57:40 +0800

    Display that it is Zero Division Error

--- calculator.py
+++ calculator.py
@@ -43,3 +43,4 @@ def calculator():
 
         except ZeroDivisionError:
             zero_division_error_message = "The Number Cannot Be Divided By Zero"
+            print("ZeroDivisionError:" + zero_division_error_message)
